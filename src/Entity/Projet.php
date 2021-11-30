3daadf502433230eb2efad0cca7fb1502faa8304
RomainVartox <romainbouchez62@gmail.com>
2021-11-30 22:20:47 +0100

    Ajout de l'entité Projet

---
+++ src/Entity/Projet.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjetRepository::class)
+ */
+class Projet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomProjet;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descriptionProjet;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $lienProjet;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomProjet(): ?string
+    {
+        return $this->nomProjet;
+    }
+
+    public function setNomProjet(string $nomProjet): self
+    {
+        $this->nomProjet = $nomProjet;
+
+        return $this;
+    }
+
+    public function getDescriptionProjet(): ?string
+    {
+        return $this->descriptionProjet;
+    }
+
+    public function setDescriptionProjet(string $descriptionProjet): self
+    {
+        $this->descriptionProjet = $descriptionProjet;
+
+        return $this;
+    }
+
+    public function getLienProjet(): ?string
+    {
+        return $this->lienProjet;
+    }
+
+    public function setLienProjet(string $lienProjet): self
+    {
+        $this->lienProjet = $lienProjet;
+
+        return $this;
+    }
+}
